82394ccfee9827e43a486b6f8cbf327b24a32341
Jonathan Yu <yu021@lampext2v.it.csiro.au>
2015-08-14 16:02:57 +1000

    Updating download fuseki script

--- script/download-fuseki.sh
+++ script/download-fuseki.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-distr=`curl -silent https://www.apache.org/dist/jena/binaries/ | perl -ne '/href="(jena-fuseki-\d\.\d\.\d)-distribution.tar.gz"/ && print $1'`
-wget --quiet https://www.apache.org/dist/jena/binaries/$distr-distribution.tar.gz -O- | tar -zx --strip-components 1 -C /opt/fuseki/ $distr
+distr=`curl -silent https://www.apache.org/dist/jena/binaries/ | perl -ne '/href="(apache-jena-fuseki-\d\.\d\.\d).tar.gz"/ && print $1'`
+echo "wget --no-check-certificate --quiet https://www.apache.org/dist/jena/binaries/$distr.tar.gz -O- | tar -zx --strip-components 1 -C /opt/fuseki/ $distr"
+wget --no-check-certificate --quiet https://www.apache.org/dist/jena/binaries/$distr.tar.gz -O- | tar -zx --strip-components 1 -C /opt/fuseki/ $distr
